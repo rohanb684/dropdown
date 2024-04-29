c285693b76ce4999608d27fdda8bed9de6376038
rohanb684 <rohanb684@gmail.com>
2024-04-29 10:14:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { FaAngleDown } from "react-icons/fa";
+
+const items = ["Yes", "Probably Not"];
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState('');
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+  const handleItemClick = (item) => {
+    setSelectedItem(item);
+    setIsOpen(false); // Close dropdown when an item is clicked
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <h2>Should You use  a dropdown</h2>
+    <div className="dropdown-container" onMouseLeave={toggleDropdown}>
+    <div className="dropdown-button" onMouseEnter={toggleDropdown} >
+     <p>{selectedItem || 'Select'}</p> <FaAngleDown/>
     </div>
+    {isOpen && (
+      <ul className="dropdown-list">
+        {items.map((item, index) => (
+          <li key={index} onClick={() => handleItemClick(item)} className="dropdown-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+  </div>
   );
 }
 
